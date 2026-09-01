77c66553e3cb3c3045bdf94dceed0f078e4d0e64
XLee7 <arxbao@126.com>
2026-09-01 20:16:28 +0800

    Initial Commit

---
+++ orchard/orchardApp.swift
@@ -0,0 +1,17 @@
+//
+//  orchardApp.swift
+//  orchard
+//
+//  Created by XLee7 on 2026/9/1.
+//
+
+import SwiftUI
+
+@main
+struct orchardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
